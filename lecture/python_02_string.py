7d2e135a10104b6c658ecf3f4fb4887db6496f8c
SongGwon <144202414+SongGwon@users.noreply.github.com>
2023-09-20 16:53:44 +0900

    Update python_02_string.py

--- lecture/python_02_string.py
+++ lecture/python_02_string.py
@@ -68,3 +68,68 @@ print(msg[18:22])
 #print("msg[")
 # 역방향
 print(msg[-4:])
+
+# 4.문자열 함수
+str = "Hello World"
+
+print("="*100)
+# 4-1. len() : 문자열 함수 계산
+print(len(str))
+
+# 4-2. upper() and lower() : 대소문자 변경
+# - ID = : "ChoLong02" -> "cholong02".lower()
+# - 데이터 전처리 -> 1A, 1a -> upper() 1A 통일
+print(str.upper())
+print(str.lower())
+
+# 4-3. replace() : 문자열 내의 특정 문자를 치환
+print(str.replace("H", "j"))
+
+# 4-4. split() : 구분자를 기준으로 문자열 분할(Default: 공백)
+b = "hello world what a nice weather"
+print(b.split())
+print(b.split("w"))
+
+# 4-5. strip() : 문자열의 좌우공백 제거
+id = "                      python1004         "
+print(id)
+print(id.strip())
+
+# id = "          CHolong01    "
+# id.lower()  "     cholong01  "
+print(id.lower().strip())  #"cholong01"    '.' -> 참조연산자
+
+# 4-6. find() and rfind() : 문자열 내부의 특정 문자 위치 인덱스 출력
+print(str.find("o"))   # Hell'o' world
+print(str.rfind("o"))  # Hello W'o'rld
+print(str.find("world")) # 못찾으면 -1 출력
+print(str.find("World")) # 단어의 첫 글자 인덱스
+print(str.rfind("World")) # 단어 첫 글자 인덱스 (뒤부터)
+
+# 4-7. in() : 특정 문자열 포함하는지 확인(True, False 출력)
+print("Hi" in "Hi Python")
+
+
+print("="*100)
+# Quiz
+id1 = "abc123@gmail.com"
+id2 =  "ter@naver.com"
+id = "cherry1004@gmail.com"
+
+# print(val) # cherry1004 출력
+print(id.find("c"))
+print(id.find("4"))
+val = id[:10]
+# val = id[id.find("c"):id.find("4")+1]
+print(val)
+
+val = id2[:id2.find("@")]
+print(val)
+
+#    "www.daum.net"
+#    "www.google.com"
+url = "www.naver.com"
+# print(val) 출력: google or daum or naver
+
+val = url[url.find(".")+1:url.rfind(".")]
+print(val)
